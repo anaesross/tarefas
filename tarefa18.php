a48ba92ee54304d55d2683d05aa639341e9efa41
Anaê Soares <anae08@gmail.com>
2019-10-16 16:22:19 -0300

    adicionado novas tarefas para fazer

---
+++ tarefa18.php
@@ -0,0 +1,41 @@
+<?php
+/*PHP ESTRUTURADO - FILE UPLOAD
+1. Criar dois arquivos chamados: upload.php e download.php
+a. upload.php deve ter um botão para fazer upload de um arquivo no
+servidor pelo método post e você precisa salvá-lo em uma pasta
+chamada “uploads”.
+OBS: caso já exista um arquivo com o mesmo nome do que você
+está enviando, o usuário precisa ser avisado e não pode salvar o
+arquivo.
+b. download.php deve mostrar um link que ao clicar faça download
+do arquivo enviado anteriormente pelo usuário
+DICA: atributo download da tag <a>.*/
+
+/*
+2. Crie um sistema de cadastro (página de registro que redireciona para
+página que exibe todos os usuários cadastrados). No cadastro é
+necessário que o usuário possa enviar imagem de perfil e que cada
+usuário receba um id.
+DICA: salvar cadastro de usuário em um arquivos .json para possibilitar
+consultas à cadastros posteriores.
+Recomendamos trocar o nome do arquivo enviado (nome quando é salvo) pelo
+id do usuário.
+*/
+
+/*
+3. Na página de cadastro, adicionar um campo para fazer upload do CV
+(currículo) do usuário. Levar em conta as mesmas considerações que as
+do exercício anterior (salvar pelo id)*/
+
+/*
+4. Caso ainda não tenha feito isso, criar um arquivo perfil.php que exibe e
+permite editar as informações de cada um dos usuários. Para isso, é
+necessário acessar por meio da URL perfil.php?id=8 (que neste caso é um
+exemplo onde pedimos para mostrar informações do usuário com o id 8)*/
+
+/*
+5. Adicionar a perfil.php as possibilidades baixar os arquivos Imagem de
+perfil e CV.*/
+
+
+?>
